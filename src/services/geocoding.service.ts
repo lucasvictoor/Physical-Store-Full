7ff5a27702b39aeec6bff73eff75c8493beb7783
lucas8790 <lv8790121@gmail.com>
2025-01-06 15:50:50 -0300

    refactor: refatoração service Geocoding API

--- src/services/geocoding.service.ts
+++ src/services/geocoding.service.ts
@@ -7,20 +7,25 @@ export class GeocodingService {
   constructor(private readonly httpService: HttpService) {}
 
   async getCoordinates(
-    address: string,
+    query: string,
   ): Promise<{ latitude: number; longitude: number }> {
     try {
       const apiKey = process.env.GOOGLE_API_KEY;
+
       const response = await firstValueFrom(
         this.httpService.get(
           `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-            address,
+            query,
           )}&key=${apiKey}`,
         ),
       );
 
+      console.log('Google Geocoding Response:', response.data);
+
       if (response.data.status !== 'OK') {
-        throw new Error('Erro na geocodificação.');
+        throw new Error(
+          `Erro na geocodificação: ${response.data.status} - ${response.data.error_message || ''}`,
+        );
       }
 
       const location = response.data.results[0].geometry.location;
@@ -29,6 +34,10 @@ export class GeocodingService {
         longitude: location.lng,
       };
     } catch (error) {
+      console.error(
+        `Erro ao buscar coordenadas para a consulta "${query}":`,
+        error.message,
+      );
       throw new Error(`Erro ao buscar coordenadas: ${error.message}`);
     }
   }
