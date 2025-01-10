949f8fa79215c0b094b4bc0f3c821251dc13f1f1
lucas8790 <lv8790121@gmail.com>
2025-01-10 13:35:47 -0300

    feat: adicionado logs para os services de APIs

--- src/modules/store/controllers/services/viacep.service.ts
+++ src/modules/store/controllers/services/viacep.service.ts
@@ -1,25 +1,31 @@
 import { firstValueFrom } from 'rxjs';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class ViaCepService {
+  private readonly logger = new Logger(ViaCepService.name);
+
   constructor(private readonly httpService: HttpService) {}
 
   async getAddress(cep: string): Promise<{ logradouro: string; localidade: string; uf: string }> {
     try {
+      this.logger.log(`Buscando endereços pelo CEP: ${cep}`);
       const response = await firstValueFrom(this.httpService.get(`https://viacep.com.br/ws/${cep}/json/`));
 
       if (response.data.erro) {
+        this.logger.warn(`CEP inválido: ${cep}`);
         throw new Error('CEP inválido.');
       }
 
+      this.logger.log(`Sucesso ao buscar endereço para o CEP: ${cep}`);
       return {
         logradouro: response.data.logradouro,
         localidade: response.data.localidade,
         uf: response.data.uf
       };
     } catch (error) {
+      this.logger.error(`Error ao buscar endereço para o CEP: ${cep}`, error.stack);
       throw new Error(`Erro ao buscar CEP: ${error.message}`);
     }
   }
